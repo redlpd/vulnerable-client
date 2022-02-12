18b539e86c508b12c5793f81d4bebd8f968f2623
John Red Lapida <redlapida@gmail.com>
2022-02-13 00:16:31 +0800

    Added routes

---
+++ src/helper/routes.jsx
@@ -0,0 +1,13 @@
+import Login from '../pages/Login'
+import Signup from '../pages/Signup'
+
+export const publicRoutes = [
+  {
+    path: '/login',
+    component: <Login />
+  },
+  {
+    path: '/signup',
+    component: <Signup />
+  }
+]
