a6771bb45c3d426ec54c64fcfd952b0d346fd103
John Red Lapida <redlapida@gmail.com>
2022-02-13 00:15:10 +0800

    Added tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,12 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    colors: {
+      'primary-green': '#48bf53',
+      'secondary-green': '#48bf53',
+      'light-green': '#91f086',
+      white: '#FFF'
+    },
     extend: {},
   },
   plugins: [],
